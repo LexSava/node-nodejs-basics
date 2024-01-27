1f85d6e2ee0a8637068389dd7f3a4f43784f4d6e
Alexei Savostyanov <lexsava@gmail.com>
2024-01-27 14:52:12 +0100

    feat: rename.js - implement function that renames file wrongFilename.txt to properFilename with extension .md
    rename.js - implement function that renames file wrongFilename.txt to properFilename with extension .md (if there's no file wrongFilename.txt or properFilename.md already exists Error with message FS operation failed must be thrown)

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,33 @@
+import { promises as fsPromises, constants } from "fs";
+
 const rename = async () => {
-    // Write your code here 
+  const sourceFilePath = "src/fs/files/wrongFilename.txt";
+  const destinationFilePath = "src/fs/files/properFilename.md";
+
+  try {
+    await fsPromises.access(destinationFilePath, constants.F_OK);
+    console.error(
+      "\x1b[31m%s\x1b[0m",
+      "FS operation failed: Destination file already exists"
+    );
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      try {
+        await fsPromises.access(sourceFilePath, constants.R_OK);
+      } catch (error) {
+        console.error(
+          "\x1b[31m%s\x1b[0m",
+          "FS operation failed: Source file not found"
+        );
+        return;
+      }
+
+      await fsPromises.rename(sourceFilePath, destinationFilePath);
+      console.log("\x1b[32m%s\x1b[0m", "File has been successfully renamed.");
+    } else {
+      throw error;
+    }
+  }
 };
 
 await rename();
