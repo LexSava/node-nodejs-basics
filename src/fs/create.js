4d3eb360d405cb6788f5821cccb3e258309802c8
Alexei Savostyanov <lexsava@gmail.com>
2024-01-27 13:44:45 +0100

    feat: create function in create.js file
    create.js - implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { access, writeFile } from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = "src/fs/files/fresh.txt";
+  try {
+    await access(filePath);
+    throw new Error("FS operation failed: File already exists");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await writeFile(filePath, "I am fresh and young");
+      console.log("File fresh.txt has been created successfully.");
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
