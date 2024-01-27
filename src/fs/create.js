0ba17a32137a663b61b758a7eb64e08c59997ac6
Alexei Savostyanov <lexsava@gmail.com>
2024-01-27 14:29:59 +0100

    feat: add console colors

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,11 +4,17 @@ const create = async () => {
   const filePath = "src/fs/files/fresh.txt";
   try {
     await access(filePath);
-    throw new Error("FS operation failed: File already exists");
+    console.error(
+      "\x1b[31m%s\x1b[0m",
+      "FS operation failed: File already exists"
+    );
   } catch (error) {
     if (error.code === "ENOENT") {
       await writeFile(filePath, "I am fresh and young");
-      console.log("File fresh.txt has been created successfully.");
+      console.log(
+        "\x1b[32m%s\x1b[0m",
+        "File fresh.txt has been created successfully."
+      );
     } else {
       throw error;
     }
