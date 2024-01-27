f3a5c7d1786cdc4a4c3f3839bec5ec0f698e25bb
Alexei Savostyanov <lexsava@gmail.com>
2024-01-27 17:21:57 +0100

    feat: read.js - implement function that prints content of the fileToRead.txt into console
    read.js - implement function that prints content of the fileToRead.txt into console (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { promises as fsPromises, constants } from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = "src/fs/files/fileToRead.txt";
+
+  try {
+    await fsPromises.access(filePath, constants.R_OK);
+    const content = await fsPromises.readFile(filePath, "utf-8");
+    console.log("\x1b[32m%s\x1b[0m", "File content:");
+    console.log(content);
+  } catch (error) {
+    console.error(
+      "\x1b[31m%s\x1b[0m",
+      "FS operation failed: File not found or unable to read."
+    );
+  }
 };
 
 await read();
