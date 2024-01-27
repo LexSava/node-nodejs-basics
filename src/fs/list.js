a35ee77bdbc9d7f98a462d83854303755cca6e38
Alexei Savostyanov <lexsava@gmail.com>
2024-01-27 17:18:54 +0100

    feat: list.js - implement function that prints all array of filenames from files folder into console
    list.js - implement function that prints all array of filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { promises as fsPromises, constants } from "fs";
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = "src/fs/files";
+
+  try {
+    await fsPromises.access(folderPath, constants.R_OK);
+    const files = await fsPromises.readdir(folderPath);
+    console.log("\x1b[32m%s\x1b[0m", "File names in the folder:");
+    console.log(files);
+  } catch (error) {
+    console.error(
+      "\x1b[31m%s\x1b[0m",
+      "FS operation failed: Folder not found or unable to access."
+    );
+  }
 };
 
 await list();
